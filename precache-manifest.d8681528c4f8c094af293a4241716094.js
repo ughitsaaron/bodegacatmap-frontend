f66f0134132e0773d8ddf30b06fb00aa188fda38
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-04 02:08:31 +0000

    Deploy ughitsaaron/bodegacats to github.com/ughitsaaron/bodegacats.git:gh-pages

---
+++ precache-manifest.d8681528c4f8c094af293a4241716094.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bffbbf6743ebeba7d026503c7aff31d0",
+    "url": "/bodegacats/index.html"
+  },
+  {
+    "revision": "7f678cb1b700cef78bd8",
+    "url": "/bodegacats/static/js/2.f8582a2d.chunk.js"
+  },
+  {
+    "revision": "f6656c018f1c98e6c67d",
+    "url": "/bodegacats/static/js/main.6b6fd85e.chunk.js"
+  },
+  {
+    "revision": "2f9aeaa5ab3f43cb9942",
+    "url": "/bodegacats/static/js/runtime-main.d85ecf65.js"
+  }
+]);
